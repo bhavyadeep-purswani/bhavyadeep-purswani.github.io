307b6f3f831bc148512f894ada83f9368d8b5096
Bhavyadeep <bhavyadeep.purswani98@gmail.com>
2025-01-23 23:41:58 +0530

    build update

---
+++ build/_app/immutable/nodes/0.e07ddd00.js
@@ -0,0 +1,13 @@
+import{S as v,i as L,s as E,C as G,D as S,k as g,q as T,a as j,E as $,l as h,m as w,h as c,r as C,c as J,F as q,n as z,G as f,b as D,H as I,I as N,J as P,K as W,g as Z,d as k,L as O}from"../chunks/index.a3d43963.js";import{d as y}from"../chunks/app.store.b91e8b19.js";const R=!0,A=Object.freeze(Object.defineProperty({__proto__:null,prerender:R},Symbol.toStringTag,{value:"Module"}));function F(n){let a,d,s,u,l,o,i,p;G(n[3]);const _=n[2].default,e=S(_,n,n[1],null);return{c(){a=g("script"),s=g("script"),u=T(`window.dataLayer = window.dataLayer || [];
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'G-Z8JGT3N36E');`),l=j(),e&&e.c(),this.h()},l(t){const r=$("svelte-1suwf4m",document.head);a=h(r,"SCRIPT",{src:!0});var b=w(a);b.forEach(c),s=h(r,"SCRIPT",{});var m=w(s);u=C(m,`window.dataLayer = window.dataLayer || [];
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'G-Z8JGT3N36E');`),m.forEach(c),r.forEach(c),l=J(t),e&&e.l(t),this.h()},h(){a.async=!0,q(a.src,d="https://www.googletagmanager.com/gtag/js?id=G-Z8JGT3N36E")||z(a,"src",d)},m(t,r){f(document.head,a),f(document.head,s),f(s,u),D(t,l,r),e&&e.m(t,r),o=!0,i||(p=I(window,"resize",n[3]),i=!0)},p(t,[r]){e&&e.p&&(!o||r&2)&&N(e,_,t,t[1],o?W(_,t[1],r,null):P(t[1]),null)},i(t){o||(Z(e,t),o=!0)},o(t){k(e,t),o=!1},d(t){c(a),c(s),t&&c(l),e&&e.d(t),i=!1,p()}}}function H(n,a,d){let s;O(n,y,i=>d(0,s=i));let{$$slots:u={},$$scope:l}=a;function o(){y.set(s=window.innerWidth)}return n.$$set=i=>{"$$scope"in i&&d(1,l=i.$$scope)},[s,l,u,o]}class B extends v{constructor(a){super(),L(this,a,H,F,E,{})}}export{B as component,A as universal};
