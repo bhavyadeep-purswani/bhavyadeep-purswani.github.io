4b2581d1b9ebf9abfd525f651736179e702636da
Bhavyadeep <bhavyadeep.purswani98@gmail.com>
2025-01-23 23:32:31 +0530

    Updates

---
+++ _app/immutable/chunks/app.store.97f6ed68.js
@@ -0,0 +1,1 @@
+import{d as t,w as E}from"./index.e8e12666.js";const n=!0,o=n;var s=(r=>(r[r.MOBILE=0]="MOBILE",r[r.TABLET=1]="TABLET",r[r.DESKTOP=2]="DESKTOP",r))(s||{});const a=E(0),O=t([a],([r])=>r>1024||!o?s.DESKTOP:r>640?s.TABLET:s.MOBILE);export{s as D,a as d,O as w};
