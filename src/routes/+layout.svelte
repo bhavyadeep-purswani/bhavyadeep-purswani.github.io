d1523703e82c6ea8d7b2f1e20bf308b259353c8d
Bhavyadeep <bhavyadeep.purswani98@gmail.com>
2025-01-23 23:41:41 +0530

    add google analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,5 +3,17 @@
 	import { deviceWidth } from '../app.store';
 </script>
 
+<svelte:head>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-Z8JGT3N36E"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'G-Z8JGT3N36E');
+	</script>
+</svelte:head>
 <svelte:window bind:innerWidth={$deviceWidth} />
 <slot />
